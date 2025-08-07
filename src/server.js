2fdb5a5a3daba1e0a998f1c8f086c4f09921e23c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-08-07 20:15:23 -0300

    feat: capture method and url in req

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import http from "node:http"
 
 const server = http.createServer((req, res)=>{
+  const {method, url} = req
+  console.log(method, url)
   return res.end("hello world")
 })
 server.listen(3333)
