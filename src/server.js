c7b9e006e5f67f9944815416a89eec53a82ee598
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-08-17 20:53:03 -0300

    feat: add database class

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import http from "node:http"
 import { json } from "./middlewares/json.js"
+import { Database } from "./database.js"
 
-const users = []
+const database = new Database()
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users")
     return res
       .setHeader('content-type', 'application/json')
       .end(JSON.stringify(users))
@@ -20,7 +22,7 @@ const server = http.createServer(async (req, res) => {
       name: name,
       email: email
     }
-    users.push(user)
+    database.insert("users", user)
     return res.writeHead(201).end()
   }
 })
