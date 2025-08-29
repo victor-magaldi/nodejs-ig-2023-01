996e01b9e95fb191592e7df8fae7f0ba02c4ebff
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-08-28 23:17:07 -0300

    feat: implement persist local db in json

--- src/database.js
+++ src/database.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises'
+
+const dataBasePath = new URL('db.json', import.meta.url)
 export class Database {
   #database = {}
+  constructor() {
+    fs.readFile(dataBasePath, 'utf-8').then(data => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+  #persist() {
+    fs.writeFile(dataBasePath, JSON.stringify(this.#database))
+  }
   select(table) {
     const data = this.#database[table]
 
@@ -11,7 +24,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
     return data
   }
-
 }
