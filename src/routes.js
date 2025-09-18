0f6e3d2a066a59589520081c7f49bf7503ef2167
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-09-17 22:13:48 -0300

    feat: add Update User

--- src/routes.js
+++ src/routes.js
@@ -34,9 +34,19 @@ export const routes = [
     method: "DELETE",
     path: buildRoutePath("/users/:id"),
     handler: (req, res) => {
-      console.log("req", req.params)
       const { id } = req.params
       database.delete("users", id)
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+      database.update("users", id, { name, email })
 
       return res.writeHead(204).end()
     }
